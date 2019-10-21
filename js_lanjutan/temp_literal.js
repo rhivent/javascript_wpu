377bb763f88bc792973814f0314a0430df4c9027
rhivent <zzzzjrjr@gmail.com>
2019-10-21 22:14:46 +0700

    template literal

---
+++ js_lanjutan/temp_literal.js
@@ -0,0 +1,19 @@
+// single line
+console.log(`string text`);
+
+
+// multi line
+console.log(`string text
+string text
+string text
+string text`);
+
+
+// embedded expression
+// console.log(`string text ${expression} string text`);
+// console.log(`${alert('haa')}`);
+const x =10;
+console.log(`${(x % 2 == 0) ? 'genap' : 'ganjil'}`)
+
+// tagged template
+// tag `string text ${expression} string text`
