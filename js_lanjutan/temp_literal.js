2ccd8fd93d14183beae5a8c19cacfb1a69a7be5a
rhivent <zzzzjrjr@gmail.com>
2019-10-26 06:46:33 +0700

    template literal:latihan

--- js_lanjutan/temp_literal.js
+++ js_lanjutan/temp_literal.js
@@ -17,3 +17,82 @@ console.log(`${(x % 2 == 0) ? 'genap' : 'ganjil'}`)
 
 // tagged template
 // tag `string text ${expression} string text`
+
+
+
+/* HTML FRAGMENT */ 
+const hehe = [
+	{
+		nama:'Riventus AHA',
+		umur:24,
+		nim:'33242',
+		email:'ventusmn@gmail.com'
+	},{
+		nama:'Riventus AHA2',
+		umur:24,
+		nim:'45211',
+		email:'ventusmn@gmail.com'
+	},{
+		nama:'Riventus AHA3',
+		umur:24,
+		nim:'673452323',
+		email:'ventusmn@gmail.com'
+	},{
+		nama:'Riventus AHA4',
+		umur:24,
+		nim:'897844900',
+		email:'ventusmn@gmail.com'
+	}
+]
+
+// const elmnt = `<div class="nama">
+// 	<h2>${hehe.nama}</h2>
+// 	<span class="nim">${hehe.nim}<span>
+// </div>`;
+
+
+// menggunakan metode join utk menghilangkan tanda ,
+// const elmnt = `<div class="nama">
+// 	${hehe.map(m => `<h2>${m.nama}</h2>
+// 	<span class="nim">${m.nim}<span>`).join('')}
+// </div>`;
+
+
+// conditional 
+// const lagu = {
+// 	title:'ehheheehe',
+// 	singer:'qwieoqiwoeiqwoe'
+// }
+
+// const elmnt = `<div class="title">
+// 	<h2>${lagu.title}</h2>
+// 	<span class="singer">${lagu.singer} ${lagu.feat ? `(feat. ${lagu.feat})` : ''}<span>
+// </div>`;
+
+
+// netsted
+const mhs2 = 
+	{
+		nama:'Riventus AHA',
+		umur:24,
+		nim:'33242',
+		mataKuliah:[
+		'Matkul1',
+		'Matkul2',
+		'Matkul3',
+		'Matkul4',
+		'Matkul5',
+		]
+	}
+
+function cetakMatKul(matkul){
+	return `${matkul.map(m => (`<p>${m}</p>`)).join('')}`
+}
+
+const elmnt = `<div class="nama">
+	<h2>${mhs2.nama}</h2>
+	<span class="nim">${mhs2.nim}<span>
+	<span class="matkul">${cetakMatKul(mhs2.mataKuliah)}<span>
+</div>`;
+
+document.body.innerHTML = elmnt;
